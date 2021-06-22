1e5d2a0196f4d4c023d8a6d3e658f8e3d3e21762
xing-zhi, jiang <a97410985new@gmail.com>
2021-06-22 21:01:35 +0800

    change app color

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default class App extends Vue {}
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #1a63ac;
   margin-top: 60px;
 }
 </style>
