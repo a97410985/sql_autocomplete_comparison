f2a25b65bb22fab59ce75050bd4d5a137e7640ba
xing-zhi, jiang <a97410985new@gmail.com>
2021-06-22 18:27:07 +0800

    change deploy setting

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:a97410985/sql_autocomplete_comparison.git main:gh-pages
 
 cd -
